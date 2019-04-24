aa1f2e1387e3bfb5f165462bf02e5078f0487098
Ivan Lukic <i.lukic@progresso-group.de>
2019-04-24 15:41:28 +0200

    Write "no changelog available" if no changelog for the tag can be found

--- changelogParser.js
+++ changelogParser.js
@@ -9,20 +9,25 @@ exports.getDescription = function(path, tag) {
     // first find the start of the changelog entry
     // e.g. ## [1.6.3] - 2019-04-09
     // and select everything after that
-    contents = contents.match(`## \\[${tag}\\][\\s\\S]*`)[0];
-    var startIndex = contents.indexOf('###');
+    var matches = contents.match(`## \\[${tag}\\][\\s\\S]*`);
+    if (matches !== null) {
+        contents = matches[0];
+        var startIndex = contents.indexOf('###');
 
-    // find the end, which is normally the start of the next
-    // changelog entry
-    var endIndex = contents.indexOf('## [', 1);
+        // find the end, which is normally the start of the next
+        // changelog entry
+        var endIndex = contents.indexOf('## [', 1);
 
-    // if end is not found, it is the only or first changelog version
-    if (endIndex === -1) {
-        contents = contents.substr(startIndex);
+        // if end is not found, it is the only or first changelog version
+        if (endIndex === -1) {
+            contents = contents.substr(startIndex);
+        } else {
+            var length = endIndex - startIndex;
+            contents = contents.substr(startIndex, length);
+        }
     } else {
-        var length = endIndex - startIndex;
-        contents = contents.substr(startIndex, length);
+        contents = "No changelog available"
     }
-    
+
     return contents.trim();
 }

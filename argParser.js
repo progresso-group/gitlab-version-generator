e4d2ed2ae55393dd3fcf8ffc8adfd705fce94c41
Ivan Lukic <i.lukic@progresso-group.de>
2019-04-24 11:00:03 +0200

    Increase version to 1.1.3

--- argParser.js
+++ argParser.js
@@ -1,7 +1,7 @@
 exports.parseArgs = function() {
     var ArgumentParser = require('argparse').ArgumentParser;
     var parser = new ArgumentParser({
-        version: '1.1.2',
+        version: '1.1.3',
         addHelp:true,
         description: 'Gitlab release generator'
       });
